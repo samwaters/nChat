42befe6a22b1d7143f0a780be82cbf58d9af2166
Sam Waters <sam@samwaters.com>
2016-11-11 16:34:07 +0000

    Update navigation component to have themes

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ThemeService} from '../../services/theme.service';
+import {BehaviorSubject} from 'rxjs';
 
 @Component({
   selector: 'app-navigation',
@@ -8,13 +10,27 @@ import { Component, OnInit } from '@angular/core';
 export class NavigationComponent implements OnInit {
   public links:Array<any> = [];
   public title:string = 'ng2-websocket';
-  constructor() {
+  public theme:string;
+
+  constructor(private themeService:ThemeService) {
     this.links = [
       {href:'#', title:'Link 1'},
       {href:'#', title:'Link 2'},
       {href:'#', title:'Link 3'},
     ]
+    this.themeService.currentTheme.subscribe((theme:string) => {
+      this.theme = theme;
+    })
   }
+
+  public get themes():Array<any> {
+    return this.themeService.themeList;
+  }
+
+  public changeTheme(className:string) {
+    this.themeService.changeTheme(className);
+  }
+
 
   ngOnInit() {
   }
