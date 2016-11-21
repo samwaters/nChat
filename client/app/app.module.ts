d168d360870d97a42caff29b4c3a88bb2cbd35a7
Sam Waters <sam@samwaters.com>
2016-11-21 12:03:40 +0000

    Standardize app module

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,75 +1,98 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+// Components
+import {AppComponent} from './app.component';
+import {AuthComponent} from './components/auth/auth.component';
+import {CaptchaComponent} from './components/captcha/captcha.component';
+import {ChatComponent} from './components/chat/chat.component';
+import {LoginComponent} from './components/login/login.component';
+import {MaterialInputComponent} from './components/input/material-input/material-input.component';
+import {MessageComponent} from './components/message/message.component';
+import {MessagingComponent} from './components/messaging/messaging.component';
+import {MessagingInputComponent} from './components/input/messaging-input/messaging-input.component';
+import {NavigationComponent} from './components/navigation/navigation.component';
+import {SignupComponent} from './components/signup/signup.component';
+import {UserComponent} from './components/user/user.component';
+import {UsersComponent} from './components/users/users.component';
 
-import { AppComponent } from './app.component';
-import { UsersComponent } from './components/users/users.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
-import { MessagingComponent } from './components/messaging/messaging.component';
-import {UserService} from './services/user.service';
-import {WebsocketService} from './services/websocket.service';
+// Directives
+import {FlexDirective, LayoutDirective} from './directives/flex.directive';
+
+// Effects
+import {EffectsModule} from '@ngrx/effects';
+import {MessageEffects} from './effects/message.effects';
+import {UserEffects} from './effects/user.effects';
+
+// Guards
+import {ChatAuthGuard} from './guards/chat.guard';
+
+// Modules
+import {AngularFireModule} from 'angularfire2';
+import {BrowserModule} from '@angular/platform-browser';
 import {MaterialModule} from '@angular/material';
 import {StoreModule, combineReducers} from '@ngrx/store';
-import {storeLogger} from 'ngrx-store-logger';
-import {userReducer} from './reducers/user.reducer';
-import {compose} from '@ngrx/core/compose';
-import {FlexDirective, LayoutDirective} from './directives/flex.directive';
-import { UserComponent } from './components/user/user.component';
-import {EffectsModule} from '@ngrx/effects';
-import {UserEffects} from './effects/user.effects';
-import { MessagingInputComponent } from './components/input/messaging-input/messaging-input.component';
-import {MessageService} from './services/message.service';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {NgModule} from '@angular/core';
+
+// Reducers
 import {messageReducer} from './reducers/message.reducer';
-import {MessageEffects} from './effects/message.effects';
-import { MessageComponent } from './components/message/message.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { LoginComponent } from './components/login/login.component';
-import { ChatComponent } from './components/chat/chat.component';
-import { AuthComponent } from './components/auth/auth.component';
-import {routing} from './app.routes';
-import {ThemeService} from './services/theme.service';
-import {AngularFireModule} from 'angularfire2';
-import {firebaseConfig, firebaseAuthConfig} from '../../config/firebase.config';
-import {FirebaseService} from './services/firebase.service';
-import {ChatAuthGuard} from './guards/chat.guard';
-import { CaptchaComponent } from './components/captcha/captcha.component';
-import {CaptchaService} from './services/captcha.service';
-import { MaterialInputComponent } from './components/input/material-input/material-input.component';
+import {userReducer} from './reducers/user.reducer';
+
+// Services
 import {ApiService} from './services/api.service';
+import {CaptchaService} from './services/captcha.service';
+import {FirebaseService} from './services/firebase.service';
+import {MessageService} from './services/message.service';
+import {WebsocketService} from './services/websocket.service';
+import {ThemeService} from './services/theme.service';
+import {UserService} from './services/user.service';
+
+// Other
+import {compose} from '@ngrx/core/compose';
+import {firebaseConfig, firebaseAuthConfig} from '../../config/firebase.config';
+import {routing} from './app.routes';
+import {storeLogger} from 'ngrx-store-logger';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    NavigationComponent,
-    MessagingComponent,
-    FlexDirective,
-    LayoutDirective,
-    UserComponent,
-    MessagingInputComponent,
-    MessageComponent,
-    SignupComponent,
-    LoginComponent,
-    ChatComponent,
     AuthComponent,
     CaptchaComponent,
-    MaterialInputComponent
+    ChatComponent,
+    FlexDirective,
+    LayoutDirective,
+    LoginComponent,
+    MaterialInputComponent,
+    MessagingComponent,
+    MessagingInputComponent,
+    MessageComponent,
+    NavigationComponent,
+    SignupComponent,
+    UserComponent,
+    UsersComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig),
     BrowserModule,
+    EffectsModule.run(MessageEffects),
+    EffectsModule.run(UserEffects),
     FormsModule,
     HttpModule,
-    routing,
     MaterialModule.forRoot(),
-    EffectsModule.run(UserEffects),
-    EffectsModule.run(MessageEffects),
+    routing,
     StoreModule.provideStore(
       compose(storeLogger(), combineReducers)({messages:messageReducer, users: userReducer})
-    ),
-    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig)
+    )
   ],
-  providers: [UserService, WebsocketService, MessageService, ThemeService, FirebaseService, CaptchaService, ApiService, ChatAuthGuard],
+  providers: [
+    ApiService,
+    CaptchaService,
+    ChatAuthGuard,
+    FirebaseService,
+    MessageService,
+    ThemeService,
+    WebsocketService,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
