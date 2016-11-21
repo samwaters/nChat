058d0824bb08f74901bf084401b61ea7dc9aaa87
Sam Waters <sam@samwaters.com>
2016-11-21 17:26:41 +0000

    Start settings dialog for displaying and re-generating keys

--- client/app/components/dialogs/settings-dialog/settings-dialog.component.ts
+++ client/app/components/dialogs/settings-dialog/settings-dialog.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {IAppState} from '../../../reducers/state.interface';
+import {Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+import {settingsActions} from '../../../reducers/settings.reducer';
 
 @Component({
   selector: 'app-settings-dialog',
@@ -7,7 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsDialogComponent implements OnInit {
 
-  constructor() { }
+  // Model stuff
+  private keySize:number = 1024;
+  // Store stuff
+  private settings:Observable<any>;
+  constructor(private store:Store<IAppState>) {
+    this.settings = store.select('settings');
+  }
+
+  public generateKeys() {
+    console.log('Using key size', this.keySize);
+    this.store.dispatch(settingsActions.generateClientKeyPair(this.keySize));
+  }
 
   ngOnInit() {
   }
